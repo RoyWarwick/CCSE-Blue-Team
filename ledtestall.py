f303d282b1f69e2129e9cd095f7fd0bb4ebc8ecf
Toro <61827434+Toro2631@users.noreply.github.com>
2021-02-04 17:52:04 +0000

    Tests the functionality of all LEDs

---
+++ ledtestall.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+#GPIO.cleanup()
+#time.sleep(1)
+print ("LED 17 on")
+GPIO.output(17,GPIO.HIGH)
+time.sleep(1)
+print ("LED 18 on")
+GPIO.output(18,GPIO.HIGH)
+time.sleep(1)
+print ("LED 22 on")
+GPIO.output(22,GPIO.HIGH)
+time.sleep(1)
+print ("LED 23 on")
+GPIO.output(23,GPIO.HIGH)
+time.sleep(1)
+
+GPIO.output(17,GPIO.LOW)
+GPIO.output(18,GPIO.LOW)
+GPIO.output(22,GPIO.LOW)
+GPIO.output(23,GPIO.LOW)
+
+GPIO.cleanup()
