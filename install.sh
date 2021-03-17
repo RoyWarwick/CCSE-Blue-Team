a3132e27767aef98595c38bc68c97aa3e37e3260
Murat Saglam <muratusaglam@gmail.com>
2021-03-17 15:45:55 +0000

    Updated install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ fi
 
 sudo apt-get git
 sudo apt install python3
-git clone https://github.com/RoyWarwick/CCSE-Blue-Team.git --branch GUI	#Downloads the project
+git clone https://github.com/RoyWarwick/CCSE-Blue-Team.git?raw=true --branch GUI	#Downloads the project
+sudo chown -R $USER CCSE-Blue-Team
 cd CCSE-Blue-Team
 python3 -m virtualenv venv
 source venv/bin/activate
