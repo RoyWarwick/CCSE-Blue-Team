882e0b003d49362c6f373b867f2a305ce73974b7
Murat Saglam <muratusaglam@gmail.com>
2021-03-17 15:58:53 +0000

    Updated install.sh

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # Installation instructions
 Make sure you're using a fresh install ubuntu machine
 
-1. **wget https://github.com/RoyWarwick/CCSE-Blue-Team/blob/GUI/install.sh -O install_script** in your terminal
+1. **wget https://github.com/RoyWarwick/CCSE-Blue-Team/blob/GUI/install.sh?raw=true -O install_script** in your terminal
 2. chmod +x install_script
 3. ./install_script 
 4. follow the installation wizards for the dependencies 
--- install.sh
+++ install.sh
@@ -1,18 +1,13 @@
-if [[ $EUID != 0 ]]
-then
-    echo "This script must be run as the superuser." >&2
-    echo "Exit status: 1" >&2
-    exit 1
-fi
-
+#DONT RUN THE SCRIPT USING SUDO IT MESSES WITH THE PIP ISNTALLS
 sudo apt-get git
 sudo apt install python3
-git clone https://github.com/RoyWarwick/CCSE-Blue-Team.git?raw=true --branch GUI	#Downloads the project
+git clone https://github.com/RoyWarwick/CCSE-Blue-Team.git --branch GUI	#Downloads the project
 sudo chown -R $USER CCSE-Blue-Team
 cd CCSE-Blue-Team
 python3 -m virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+python3 server.py
 
 
 
