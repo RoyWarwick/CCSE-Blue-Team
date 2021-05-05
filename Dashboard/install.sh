d9e275482def7d68b142b78b0ca84cdecde49038
Murat Saglam <42717642+Murat-U-Saglam@users.noreply.github.com>
2021-05-05 16:12:04 +0100

    Fixed to work with directory

--- Dashboard/install.sh
+++ Dashboard/install.sh
@@ -2,8 +2,6 @@
 sudo apt install git
 sudo apt install python3
 sudo apt install python3-virtualenv
-sudo chown -R $USER Dashboard
-cd Dashboard
 python3 -m virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
