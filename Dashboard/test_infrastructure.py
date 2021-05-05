7d7efce8aedec2e0b677f6f4468cc0a195c367dd
Murat Saglam <42717642+Murat-U-Saglam@users.noreply.github.com>
2021-05-05 17:27:31 +0100

    Added a few more tests.
    Added testing for real time data as well as sending data to the processor.

--- Dashboard/test_infrastructure.py
+++ Dashboard/test_infrastructure.py
@@ -1,4 +1,6 @@
 import dataHandler
+import recieveFile
+import sendFile
 
 
 def test_number_of_sensors():
@@ -20,13 +22,18 @@ def test_number_of_tunnels():
     assumed_tunnels = 2
     assert number_of_tunnels == assumed_tunnels, f"Your assumption is incorrect there are {number_of_tunnels} of tunnels"
 
+
 def test_physical_access_attempt():
     JSON_FILE = dataHandler.getPhysicalSecurityData()
     Attempts = len(JSON_FILE)
     assert 5 == Attempts, f"The website stores up to a maximum of 5 attempts, however currently it has only received {Attempts}"
 
 
+def test_real_time_data_test():
+    assert recieveFile.get_data() is not None, f"The processor is not sending real-time data."
 
 
-
-
+def test_sending_data():
+    send_to_arno = open("send_to_arno", "w")
+    send_to_arno.close()
+    assert sendFile.send_file(send_to_arno) is not None, f"The processor is not up."
