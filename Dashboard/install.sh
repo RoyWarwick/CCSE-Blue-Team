36867179c1c9f4e13d3fb06842bdfc5c88a5fd21
Murat Saglam <42717642+Murat-U-Saglam@users.noreply.github.com>
2021-05-05 15:35:51 +0100

    Fixed install.sh
    Updated the script to work with the jenkins methodology

--- Dashboard/install.sh
+++ Dashboard/install.sh
@@ -2,9 +2,8 @@
 sudo apt install git
 sudo apt install python3
 sudo apt install python3-virtualenv
-git clone https://github.com/RoyWarwick/CCSE-Blue-Team.git --branch GUI	#Downloads the project
-sudo chown -R $USER CCSE-Blue-Team
-cd CCSE-Blue-Team
+sudo chown -R $USER Dashboard
+cd Dashboard
 python3 -m virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
